3d45f4f184cc5dc2b23bcf5069ef98e648fe3bf2
John McCabe-Dansted <gmatht@gmail.com>
2015-09-25 15:17:12 +0800

    Now try to benchmark i upto 1M instead of just 100

--- src/benchmarks/do_all.sh
+++ src/benchmarks/do_all.sh
@@ -18,10 +18,10 @@ for BENCHMARK_RUNDIR in .. ../v1.0/src/
 do
 export BENCHMARK_RUNDIR
 killset
-./FLL10_formulas.sh 99 N Y  | sed s/^/-/ | timeout $SECS ./do_set.sh FLL10-NY ; killset
-./FLL10_formulas.sh 99 Y N  | sed s/^/-/ | timeout $SECS ./do_set.sh FLL10-YN ; killset
-./FLL10_formulas.sh 99 Y N  |              timeout $SECS ./do_set.sh FLL10YN  ; killset
-./FLL10_formulas.sh 99 N Y  |              timeout $SECS ./do_set.sh FLL10NY  ; killset
+./FLL10_formulas.sh 999999 N Y  | sed s/^/-/ | timeout $SECS ./do_set.sh FLL10-NY ; killset
+./FLL10_formulas.sh 999999 Y N  | sed s/^/-/ | timeout $SECS ./do_set.sh FLL10-YN ; killset
+./FLL10_formulas.sh 999999 Y N  |              timeout $SECS ./do_set.sh FLL10YN  ; killset
+./FLL10_formulas.sh 999999 N Y  |              timeout $SECS ./do_set.sh FLL10NY  ; killset
 done
 done
 
